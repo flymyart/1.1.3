c6590f4e05f55cbbfc73e41d70329c52a9b1399e
Tatiana <43416772+flymyart@users.noreply.github.com>
2023-08-25 17:19:59 +0300

    Update UserServiceImpl.java
    change file UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,39 +1,34 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-//        new UserDaoHibernateImpl().createUsersTable();
-        new UserDaoJDBCImpl().createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-//        new UserDaoHibernateImpl().dropUsersTable();
-        new UserDaoJDBCImpl().dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-//        new UserDaoHibernateImpl().saveUser(name, lastName, age);
-        new UserDaoJDBCImpl().saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-//        new UserDaoHibernateImpl().removeUserById(id);
-        new UserDaoJDBCImpl().removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-//        return new UserDaoHibernateImpl().getAllUsers();
-        return new UserDaoJDBCImpl().getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-//        new UserDaoHibernateImpl().cleanUsersTable();
-        new UserDaoJDBCImpl().cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
